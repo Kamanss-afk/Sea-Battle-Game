7c4820c620de9ef6afd57767b09ad43a669759a7
Dima <dimasmir93@gmail.com>
2022-03-24 10:20:42 +0300

    refactor: rename Toogle to Toggle

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -4,13 +4,13 @@ import { RouterModule } from '@angular/router';
 
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
-import { ToogleComponent } from './components/toogle/toogle.componnet';
+import { ToggleComponent } from './components/toggle/toggle.componnet';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     InputComponent,
-    ToogleComponent,
+    ToggleComponent,
   ],
   imports: [
     CommonModule, 
@@ -21,7 +21,7 @@ import { ToogleComponent } from './components/toogle/toogle.componnet';
     RouterModule,
     ButtonComponent,
     InputComponent,
-    ToogleComponent,
+    ToggleComponent,
   ],
 })
 export class SharedModule {}
