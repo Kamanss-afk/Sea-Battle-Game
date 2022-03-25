522a731b706f1377353744164deacb4de9cf30af
Dima <dimasmir93@gmail.com>
2022-03-25 09:52:16 +0300

    fix: add ControlValueAccessor for Input Component

--- frontend/src/app/shared/components/input/input.component.ts
+++ frontend/src/app/shared/components/input/input.component.ts
@@ -1,10 +1,43 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Self } from "@angular/core";
+import { ControlValueAccessor, NgControl } from "@angular/forms";
 
 @Component({
   selector: 'app-input',
   templateUrl: 'input.component.html',
   styleUrls: ['input.component.scss'],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() placeholder: string = '';
+
+  public value: string | undefined;
+  public disabled: boolean = false;
+
+  private onChange: (value: string) => void;
+  private onTouched: () => void;
+
+  constructor(
+    @Self() private readonly ngControl: NgControl
+  ) {
+    this.ngControl.valueAccessor = this;
+  }
+
+  public onInputValueChange(event: any): void {
+    this.onChange(event.target.value);
+  }
+
+  public writeValue(value: string): void {
+    this.value = value;
+  }
+
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 }
