565e7470e4e92c5816f14a7437a5981d74b96ec5
Dima Hmurchik <dimasmir93@gmail.com>
2022-03-28 20:13:45 +0300

    feat: Add rows of letters and numbers for the Board Component
    Add the source of letters and numbers to the board component.
    Add rows of letters and numbers to the Board Component template.
    Add styles to the board component style file

--- frontend/src/app/shared/components/board/board.component.ts
+++ frontend/src/app/shared/components/board/board.component.ts
@@ -16,6 +16,9 @@ import { Square, SquarePosition } from "../../models/square.model";
 export class BoardComponent {
   @Input() board: Array<Array<Square>> = [];
 
+  public numbers: Array<string> = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  public letters: Array<string> = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К'];
+
   @ViewChild('boardElement', {static: false}) 
   boardElement: ElementRef | undefined;
 
