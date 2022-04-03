438741c6bcbe31595800fb47e5f6c488aa74e4b1
Dima Hmurchik <dimasmir93@gmail.com>
2022-04-03 17:42:54 +0300

    feat: Add Fleet-Map Component

---
+++ frontend/src/app/pages/deploy/components/fleet-map/fleet-map.component.ts
@@ -0,0 +1,22 @@
+import { CdkDragEnd, CdkDragStart } from '@angular/cdk/drag-drop';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Ship } from '../../../../shared/models/ship.model';
+
+@Component({
+  selector: 'fleet-map',
+  templateUrl: 'fleet-map.component.html',
+  styleUrls: ['fleet-map.component.scss'],
+})
+export class FleetMapComponent {
+  @Input() deployedShips: Array<Ship> = [];
+
+  @Output() onDragStarted = new EventEmitter<{ event: CdkDragStart, ship: Ship }>()
+  dragStarted(event: CdkDragStart, ship: Ship) {
+    this.onDragStarted.emit({ event, ship });
+  }
+
+  @Output() onDragEnded = new EventEmitter<CdkDragEnd>()
+  dragEnded(event: CdkDragEnd) {
+    this.onDragEnded.emit(event);
+  }
+}
--- frontend/src/app/pages/deploy/deploy.module.ts
+++ frontend/src/app/pages/deploy/deploy.module.ts
@@ -5,12 +5,14 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { DeployComponent } from './deploy.component';
 import { FleetComponent } from './components/fleet/fleet.component';
 import { ShipComponent } from './components/ship/ship.component';
+import { FleetMapComponent } from './components/fleet-map/fleet-map.component';
 
 @NgModule({
   declarations: [
     DeployComponent,
     FleetComponent,
     ShipComponent,
+    FleetMapComponent,
   ],
   imports: [
     DragDropModule,
