f890b12143092db6fc4f2a7498c6f28f27be079c
Dima <dimasmir93@gmail.com>
2022-04-08 15:28:52 +0300

    Add the game model

---
+++ frontend/src/app/shared/models/game.model.ts
@@ -0,0 +1,44 @@
+import { Player } from './player.model';
+
+export enum GameState {
+  NONE='NONE',
+  INIT='INIT',
+  DEPLOY='DEPLOY',
+  BATTLE='BATTLE',
+  END='END',
+}
+
+export enum GameTurn {
+  PLAYER='PLAYER',
+  OPPONENT='OPPONENT',
+}
+
+export class Game {
+  state: GameState;
+  turn: GameTurn | undefined;
+  player: Player | undefined;
+  opponent: Player | undefined;
+
+  constructor() {
+    this.state = GameState.NONE;
+    this.turn = undefined;
+    this.player = undefined;
+    this.opponent = undefined
+  }
+
+  public setPlayer(name: string): void {
+    this.player = new Player(name);
+  }
+
+  public setOpponent(name: string): void {
+    this.opponent = new Player(name);
+  }
+
+  public setState(state: GameState): void {
+    this.state = state;
+  }
+
+  public setTurn(turn: GameTurn): void {
+    this.turn = turn;
+  }
+}
