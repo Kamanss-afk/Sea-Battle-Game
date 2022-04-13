3256733413937abd4fc1e17230184e1e4ddfb58c
Dima Hmurchik <dimasmir93@gmail.com>
2022-04-13 18:56:03 +0300

    Add the SocketIoModule
    Crate the socket config.
    Add the SocketIoModule to the imports of the core module.
    Add the ngx-socket-io library.

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,13 +1,18 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
 import { LayoutComponent } from './components/layout/layout.component';
 import { GameService } from './services/game.service';
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
     LayoutComponent,
   ],
-  imports: [],
+  imports: [
+    SocketIoModule.forRoot(config)
+  ],
   exports: [
     LayoutComponent,
   ],
