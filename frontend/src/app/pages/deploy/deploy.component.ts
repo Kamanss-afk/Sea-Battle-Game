490f0dfa308ff46f11f679d868e7717ef359a3a5
Dima Hmurchik <dimasmir93@gmail.com>
2022-04-14 21:46:01 +0300

    Update the deploy component
    Update the "ready" method.
    Add subscriptions to the deploy component.
    
    Subscriptions:
    -gameState
    -opponentReady
    -deployShipsSuccess
    -deployShipsError

--- frontend/src/app/pages/deploy/deploy.component.ts
+++ frontend/src/app/pages/deploy/deploy.component.ts
@@ -1,22 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { GameService } from 'src/app/core/services/game.service';
+import { Subscription } from 'rxjs';
+import { GameService } from '../../core/services/game.service';
+import { GameState } from '../../shared/models/game.model';
+import { Player } from '../../shared/models/player.model';
 import { DeployService } from './services/deploy.service';
 
 @Component({
   templateUrl: './deploy.component.html',
   styleUrls: ['./deploy.component.scss'],
 })
-export class DeployComponent {
+export class DeployComponent implements OnInit, OnDestroy {
+  private gameState: Subscription;
+  private opponentReady: Subscription;
+  private deployShipsSuccess: Subscription;
+  private deployShipsError: Subscription;
+
   constructor(
     public gameService: GameService,
     public deployService: DeployService,
     private router: Router,
   ) {}
 
+  ngOnInit(): void {
+    this.gameState = this.gameService.onGameState.subscribe(({ state }) => {
+      this.gameService.game.changeState(state);
+
+      if(state === GameState.BATTLE) this.router.navigate(['battle']);
+    });
+
+    this.opponentReady = this.gameService.onOpponentReady.subscribe(({ opponent }) => {
+      const { id, name, ready } = opponent;
+      
+      this.gameService.opponent = new Player(id, name);
+      this.gameService.opponent.ready = ready;
+    })
+
+    this.deployShipsSuccess = this.gameService.onDeployShipsSuccess.subscribe(({ ready }) => {
+      this.gameService.player.ready = ready;
+    });
+
+    this.deployShipsError = this.gameService.onDeployShipsError.subscribe(({ ready, message }) => {
+      this.gameService.player.ready = ready;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.gameState.unsubscribe();
+    this.opponentReady.unsubscribe();
+    this.deployShipsSuccess.unsubscribe();
+    this.deployShipsError.unsubscribe();
+  }
+
   public ready() {
     if(this.gameService.player.deployedShips.length == 10) {
-      this.router.navigate(['battle']);
+      const userId = this.gameService.player.id;
+      const gameId = this.gameService.game.id;
+      const ships = this.gameService.player.deployedShips.map(({ coords }) => coords);
+
+      this.gameService.deployShips(userId, gameId, ships);
     }
   }
 }
