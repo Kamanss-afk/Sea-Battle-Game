000bd43b6707483a64fbf8bbcfa28c560a095818
Dima Hmurchik <dimasmir93@gmail.com>
2022-04-18 14:56:43 +0300

    Update core module
    Add the BrowserAnimationsModule and the ToastrModule to the imports.
    Add the DeployGuard and the BattleGuard to the providers.

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,7 +1,11 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { LayoutComponent } from './components/layout/layout.component';
+import { BattleGuard } from './guards/battle.guard';
+import { DeployGuard } from './guards/deploy.guard';
 import { GameService } from './services/game.service';
 
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
@@ -11,13 +15,17 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
     LayoutComponent,
   ],
   imports: [
-    SocketIoModule.forRoot(config)
+    BrowserAnimationsModule,
+    SocketIoModule.forRoot(config),
+    ToastrModule.forRoot(),
   ],
   exports: [
     LayoutComponent,
   ],
   providers: [
     GameService,
+    DeployGuard,
+    BattleGuard
   ],
 })
 export class CoreModule {
