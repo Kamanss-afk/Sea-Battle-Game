14bd0835f9436be70036af8a70be55c3dc6584cb
Dima Hmurchik <dimasmir93@gmail.com>
2022-05-16 12:41:16 +0300

    Remove the angular router from the message component

--- frontend/src/app/shared/components/message/message.component.ts
+++ frontend/src/app/shared/components/message/message.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
 
 import { MessageConfig } from '../../../core/services/message.service';
 
@@ -12,9 +11,7 @@ export class MessageComponent {
   @Input() visible: boolean = false;
   @Input() config: MessageConfig = {} as MessageConfig;
 
-  constructor(private router: Router) {}
-
   public redirect(event: MouseEvent) {
-    this.router.navigate(['/']);
+    window.location.href = '/';
   }
 }
